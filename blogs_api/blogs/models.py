cc79fb6eb27e9d244e74c06a6e37d5669416b792
Eagle Yuan <eagleyuan21@gmail.com>
2020-05-07 12:23:26 -0400

    Finished blog api

--- blogs_api/blogs/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class blogpost(models.Model):
-    Title = models.CharField(max_length = 255)
-    Date = models.DateField(auto_now=True)
-    Image = models.ImageField(upload_to = "images/")
-    Content = models.TextField()
