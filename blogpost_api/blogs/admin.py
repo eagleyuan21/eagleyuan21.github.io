890b8e8e75ce952a6de7d7ee376cb012d9e00163
Eagle Yuan <eagleyuan21@gmail.com>
2020-05-07 21:05:40 -0400

    Move folder

---
+++ blogpost_api/blogs/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import blogpost
+
+admin.site.register(blogpost)
+
+# Register your models here.
---
+++ blogpost_api/blogs/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from .models import blogpost
+from .serializers import blogSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+class blogView(APIView):
+    def get(self, request):
+        blog = blogpost.objects.all()
+        serializer = blogSerializer(blog, many=True, context={"request": request})
+        return Response(serializer.data)
