f7342e080f0d199df3385f5d30adfe9b2b41060b
Eagle Yuan <eagleyuan21@gmail.com>
2020-05-08 16:58:13 -0400

    More mobile testing

--- assets/js/blogapi.js
+++ assets/js/blogapi.js
@@ -20,7 +20,17 @@ function myFunction(arr) {
   var img;
   var val1 = window.outerWidth/4 * 3;
   var width = val1.toString();
+  var endbreak;
   for(i = 0; i < arr.length; i++) {
+  	if(i == (arr.length - 1))
+  	{
+  		endbreak = '</p><br>';
+  	}
+  	else{
+  		endbreak = '</p><br><br><br><br><br><br><br><br><br>';
+  	}
+  	
+  	
   	if(arr[i].Image == null)
   	{
   		img = '';
@@ -29,8 +39,8 @@ function myFunction(arr) {
   		img = '<h1 class="bio-head"><img src="' + arr[i].Image + '" style="max-height:350px;max-width:500px;height:auto;width:' + width + 'px"></h1>';
   	}
   	newDate = format(arr[i].Date);
-    out += '<h1 style="font-size:31px;font-weight:bold">' + arr[i].Title + '</h1><h2 style="font-size:18px">' + newDate + '</h2>' + 
-    img + '<p style="font-size:18px;text-align:left;white-space:pre-wrap">' + arr[i].Content + '</p><br><br><br><br><br><br><br>';
+    out += '<h1 class="bio-head" style="font-size:31px;font-weight:bold;text-align:left">' + arr[i].Title + '</h1><h2 class="bio-head" style="font-size:18px;text-align:left">' + newDate + '</h2>' + 
+    img + '<p class="bio-head" style="font-size:18px;text-align:left;white-space:pre-wrap">' + arr[i].Content + endbreak;
   }
   document.getElementById("id01").innerHTML = out;
 }
