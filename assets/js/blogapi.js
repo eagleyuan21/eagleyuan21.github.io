cb1e310bb263eb6c13f42390d166c9328c61b130
Eagle Yuan <eagleyuan21@gmail.com>
2020-05-08 12:24:22 -0400

    Finished json parsing and display

--- assets/js/blogapi.js
+++ assets/js/blogapi.js
@@ -1,24 +1,34 @@
 var url = "https://blogpostseha.herokuapp.com/?format=json";
-var testurl = "https://ipinfo.io/json";
 var xhr = new XMLHttpRequest();
 
 function getObjects()
 {
-	xhr.open('GET', url, true);
+	xhr.onreadystatechange = function() {
+  		if (this.readyState == 4 && this.status == 200) {
+    		var myArr = JSON.parse(this.responseText);
+    		myFunction(myArr);
+  		}
+	};
+	xhr.open("GET", url, true);
 	xhr.send();
-	
-	xhr.onreadystatechange = processRequest;
 	
 }
 
-function processRequest(e) {
-    var i;
-    if (xhr.readyState == 4 && xhr.status == 200) 
-    {
-        var response = JSON.parse(xhr.responseText);
-        for(i = 0; i < response.length; i++)
-        {
-        	console.log(response[i].Title);
-        }
-    }
+function myFunction(arr) {
+  var out = "";
+  var i;
+  var img;
+
+  for(i = 0; i < arr.length; i++) {
+  	if(arr[i].Image == null)
+  	{
+  		img = '';
+  	}
+  	else{
+  		img = '<h1 class="bio-head"><img src="' + arr[i].Image + '" style="max-height:350px;max-width:500px;height:auto;width:auto"></h1><br>';
+  	}
+    out += '<h1 class="bio-head" style="font-size:31px;">' + arr[i].Title + '</h1><h2 class="bio-head" style="font-size:18px;text-align:left">' + arr[i].Date + '</h2><br>' + 
+    img + '<p style="font-size:16px;text-align:left;white-space:pre-wrap">' + arr[i].Content + '</p><br><br><br><br><br><br>';
+  }
+  document.getElementById("id01").innerHTML = out;
 }
