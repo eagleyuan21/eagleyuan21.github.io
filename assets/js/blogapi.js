4a487a88ffa73e56e4c6fe455cafc8415c8f4206
Eagle Yuan <eagleyuan21@gmail.com>
2020-05-08 12:52:46 -0400

    Formatting blog display

--- assets/js/blogapi.js
+++ assets/js/blogapi.js
@@ -27,8 +27,22 @@ function myFunction(arr) {
   	else{
   		img = '<h1 class="bio-head"><img src="' + arr[i].Image + '" style="max-height:350px;max-width:500px;height:auto;width:auto"></h1><br>';
   	}
-    out += '<h1 class="bio-head" style="font-size:31px;">' + arr[i].Title + '</h1><h2 class="bio-head" style="font-size:18px;text-align:left">' + arr[i].Date + '</h2><br>' + 
-    img + '<p style="font-size:16px;text-align:left;white-space:pre-wrap">' + arr[i].Content + '</p><br><br><br><br><br><br>';
+  	newDate = format(arr[i].Date);
+    out += '<h1 class="bio-head" style="font-size:31px;font-weight:bold">' + arr[i].Title + '</h1><h2 class="bio-head" style="font-size:18px;text-align:left">' + newDate + '</h2><br>' + 
+    img + '<p style="font-size:18px;text-align:left;white-space:pre-wrap">' + arr[i].Content + '</p><br><br><br><br><br><br><br>';
   }
   document.getElementById("id01").innerHTML = out;
+}
+
+function format(inputDate){ 
+    var splitDate = inputDate.split('-');
+    if(splitDate.count == 0){
+        return null;
+    }
+
+    var year = splitDate[0];
+    var month = splitDate[1];
+    var day = splitDate[2]; 
+
+    return month + '\\' + day + '\\' + year;
 }
